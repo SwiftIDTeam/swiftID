96e7453f1418f7f682c19d6fcfdfd8b52133c591
Prestonh99 <53411072+Prestonh99@users.noreply.github.com>
2024-04-11 20:56:47 -0400

    Updated SRC folder
    Updated main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include <RFID.h>
 
 
-#define GPIO_INT_PIN 000000 //******** REPLACE THIS WITH PIN WE CONNECT BUTTON TO*******
+#define GPIO_BUTTON_PIN 000000 //******** REPLACE THIS WITH PIN WE CONNECT BUTTON TO*******
 
 // USING VSPI pins (Could also use HSPI miso= 12, mosi= 13, ss= 15 , sck= 14)
 
@@ -26,7 +26,7 @@ volatile bool scanRequested = false;
 
 int serNum[5]; // stores scanned RFID
 
-void IRAM_ATTR handleInterrupt() { //ISR routine (sets flag)
+void IRAM_ATTR buttonISR() { //ISR routine (sets flag)
   scanRequested = true;
 }
 
@@ -39,15 +39,19 @@ void Init_SPI() {
   pinMode(RFID_RST_PIN, OUTPUT);
   
   // Attach interrupt to the VSPI interrupt pin (not used in VSPI, but placeholder)
-  attachInterrupt(digitalPinToInterrupt(GPIO_INT_PIN), handleInterrupt, FALLING);
+  attachInterrupt(digitalPinToInterrupt(GPIO_BUTTON_PIN), buttonISR, FALLING);
 
   // Reset the RFID reader
+  rfid.reset();
+  //******************* OLD RESET CODE *************
+  /*
   digitalWrite(RFID_RST_PIN, HIGH);
   delay(100);
   digitalWrite(RFID_RST_PIN, LOW);
   delay(100);
   digitalWrite(RFID_RST_PIN, HIGH);
   delay(100);
+  */
 
   // Set SS pin to high (deselect)
   digitalWrite(RFID_SS_PIN, HIGH);
@@ -64,6 +68,8 @@ void setup() {
 
   // Set LED to output
   pinMode(LED_BUILTIN, OUTPUT);
+  // Set button gpio pin to input ******************************** NEED TO GET BUTTON AND FIGURE OUT WHAT PIN TO CONNECT TO
+  //pinMode(GPIO_INT_PIN, INPUT);
 
   // Call init_SPI function to initialize SPI communication
   Init_SPI();
@@ -83,33 +89,45 @@ void loop() {
       if(rfid.readCardSerial()){ //reads the number from the card
 
         // Wait for scan to complete (we can probably set some interrupt for this but if not a delay() function will work)
-        delay(1000); // Adjust delay as needed
+        delay(100); // Adjust delay as needed
+
+        // move into our serNum[] variable outside the rfid class so we can do more processing and send via bluetooth
+        for (int i = 0; i < 5; i++) {
+          serNum[i] = rfid.serNum[i];
+        }
+
         //print card number
-        Serial.print(rfid.serNum[0]);
-        Serial.print(" ");
-        Serial.print(rfid.serNum[1]);
-        Serial.print(" ");
-        Serial.print(rfid.serNum[2]);
-        Serial.print(" ");
-        Serial.print(rfid.serNum[3]);
-        Serial.print(" ");
-        Serial.print(rfid.serNum[4]);
+        Serial.print("Detected Card: ");
+        for (int i = 0; i < 5; i++) {
+          Serial.print(serNum[i]);
+          Serial.print(" ");
+        }
         Serial.println("");
       }
+      else{
+        Serial.print("Error: Unable To Read Card Serial Number");
+      }
 
+    }
+    else{
+      Serial.print("Error: No Card Detected");
     }
 
     // Deselect RFID reader
     digitalWrite(RFID_SS_PIN, HIGH);
 
-    // Process RFID data
-    // Example:
-    // Serial.println(data);
-
     //turn off LED
     digitalWrite(LED_BUILTIN, LOW);
 
     rfid.halt(); //stop RFID reader
+
+    // From here we can implement some varification or something using the Serial Number 
+    // For instance, if we had access to database we would check that its in the schools database, etc...
+
+    // Process RFID data
+    // Example:
+    // Serial.println(data);
+
   }
 }
 
